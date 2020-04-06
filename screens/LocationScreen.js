1d453f5d2185f7a6e82d659f25be0cfd9f682447
e_launay <ewen.launay889@gmail.com>
2020-04-06 13:42:35 +0200

    3eme comit

--- screens/LocationScreen.js
+++ screens/LocationScreen.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import {SearchBar} from "react-native-elements";
+
 
 class LocationScreen extends Component {
+    state = {
+        search: '',
+    };
+
+    updateSearch = search => {
+        this.setState({ search });
+    };
+
     render() {
+        const { search } = this.state;
+
         return (
-            <div>
-                
-            </div>
+            <SearchBar
+                placeholder="Rechercher"
+                onChangeText={this.updateSearch}
+                value={search}
+            />
         );
     }
 }
